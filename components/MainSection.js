a9dad9604d4b8594dd280b7a9017d8784db9643e
Jakub Cerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-04-23 17:52:52 +0930

    Add files via upload

---
+++ components/MainSection.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import travelData from '../data.js'
+
+console.log(travelData)
---
+++ components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <i className="nav__icon fa-solid fa-earth-oceania"></i>
+            <span className="nav__text">my travel journal.</span>
+        </nav>
+    )
+}
